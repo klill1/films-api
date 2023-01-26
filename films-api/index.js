1bdf02389e539bcaf9f982041c8d23b21908f53f
Opilane TTHK <opilane@tthk.ee>
2023-01-26 15:20:11 +0200

    removedOldFilms

--- films-api/index.js
+++ films-api/index.js
@@ -3,10 +3,6 @@ const app = require("express")()
 const mariadb = require("mariadb")
 const port = process.env.APP_PORT
 
-app.get('/films', (req, res) => {
-    res.send(["The Lord of the Rings", "The Hobbit"])
-})
-
 const swaggerUI = require("swagger-ui-express")
 const swaggerDocument = require("./docs/swagger.json")
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
