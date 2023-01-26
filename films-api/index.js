938cf4e271cc81cf8f4f158b6519bb2d58d7bd3a
Opilane TTHK <opilane@tthk.ee>
2023-01-26 14:34:47 +0200

    ConnectionToDatabase

--- films-api/index.js
+++ films-api/index.js
@@ -1,14 +1,33 @@
 require("dotenv").config()
 const app = require("express")()
+const mariadb = require("mariadb")
 const port = process.env.APP_PORT
-
-app.get('/films', (req, res) => {
-    res.send(["The Lord of the Rings", "The Hobbit"])
-})
 
 const swaggerUI = require("swagger-ui-express")
 const swaggerDocument = require("./docs/swagger.json")
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    connectionLimit: 5
+})
+
+app.get('/customers', async (req, res) => {
+    let connection;
+    try {
+        connection = await pool.getConnection()
+        const rows = await connection.query("SELECT * FROM customers")
+        console.log(rows)
+        res.send(rows)
+    } catch (error) {
+        throw error
+    } finally {
+        if(connection) return connection.end()
+    }
+})
 
 app.listen(port,()=>{
     console.log(`Api up at: http://localhost:${port}`)
