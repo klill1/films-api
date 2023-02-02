2d4de359c114f2fe0f576c8d3da327b81b8b6c24
Opilane TTHK <opilane@tthk.ee>
2023-02-02 14:15:34 +0200

    getById

--- films-api/controllers/FilmsController.js
+++ films-api/controllers/FilmsController.js
@@ -5,3 +5,12 @@ exports.getAll = async (req, res) => {
     const films = await Film.findAll({attributes:["filmName"]})
     res.send(films)
 }
+
+exports.getById = async (req,res) => {
+    const films = await Film.findByPk(req.params.id)
+    if (films === null) {
+        res.status(404).send({"Error":"Film not found"})
+        return
+    }
+    res.send(films)
+}
