8652fc90409737ecdd3c9ac64b66b82561e87f08
Opilane TTHK <opilane@tthk.ee>
2023-02-09 13:17:13 +0200

    routesAndsSwaggerFixes

--- films-api/routes/app_routes.js
+++ films-api/routes/app_routes.js
@@ -3,8 +3,8 @@ const filmsController = require("../controllers/FilmsController.js")
 module.exports = (app) => {
     app.route("/films")
         .get(filmsController.getAll)
-        .get(filmsController.createNew)
-    //     .post(filmsController.createNew)        // Create
+        .post(filmsController.createNew)
+        
     app.route("/films/:id")
         .get(filmsController.getById)
     
