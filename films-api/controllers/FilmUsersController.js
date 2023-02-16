d6edbae8de449bc56a11119eb2e8be2c53a4d954
klill1 <kert.lillemets@gmail.com>
2023-02-16 16:51:59 +0200

    Add sql query to get films that user has watched

--- films-api/controllers/FilmUsersController.js
+++ films-api/controllers/FilmUsersController.js
@@ -1,19 +1,20 @@
 const { db } = require("../db")
 const FilmUsers = db.filmUsers
 const Film = db.films
+const User = db.users
+const QueryTypes = db.Sequelize.QueryTypes
 
-exports.getAll = async (req,res) => {
-    const filmUsers = await FilmUsers.findAll({
-        include: { all: true},
-        logging: console.log
-    })
-    console.log(filmUsers);
-    let result = []
-    result = filmUsers.map((gp) => {
-        return {
-            "filmName": gp.film.filmName,
-            "userName": `${gp.user.userName}`
-        }
-    })
-    res.send(result)
-}
+exports.getAll = async (req, res) => {
+    const sql = `SELECT GROUP_CONCAT(f.filmName SEPARATOR ', ') as "Film Name",
+                          u.userName as "User Name"
+                   FROM filmUsers AS fu 
+                   INNER JOIN films AS f on f.id = fu.FilmId 
+                   INNER JOIN users AS u on u.id = fu.UserId 
+                   GROUP BY fu.UserId;`
+    const sqlResult = await db.sequelize.query(sql, { type: QueryTypes.SELECT })
+    if (sqlResult.length === 0) {
+      res.send({ error: "No films stored on users." })
+      return
+    }
+    res.send(sqlResult)
+  }
